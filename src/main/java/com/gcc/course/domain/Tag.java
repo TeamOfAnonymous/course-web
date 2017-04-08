34e8b44422c413020ad89d33f92f7bd4408961d6
WangZK <13434995010@163.com>
2017-04-08 10:05:02 +0800

    add entity
    添加tag实体类，用来关联session和article

---
+++ src/main/java/com/gcc/course/domain/Tag.java
@@ -0,0 +1,47 @@
+package com.gcc.course.domain;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by WangZK on 2017/4/8.
+ */
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(generator = "UUID2_GENERATOR")
+    private String id;
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "tag_article", joinColumns = {
+            @JoinColumn(name = "tagId", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "articleId", referencedColumnName = "id")})
+    private Set<Article> articles;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
+    }
+}
