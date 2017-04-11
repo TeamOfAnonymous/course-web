d52707e1df541bce671958c2b8984d35445c0282
huangMP <675454967@qq.com>
2017-04-11 12:32:31 +0800

    删除 courseTag

--- src/main/java/com/gcc/course/domain/Course.java
+++ src/main/java/com/gcc/course/domain/Course.java
@@ -2,7 +2,6 @@ package com.gcc.course.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.hibernate.annotations.Where;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -20,34 +19,16 @@ public class Course extends BaseEntity {
     private String description; //简介
     private String imgUrl; //图片路径
     private String prompt ; // 提示
-
     private int sortOrder ; //排列序号，表示同级类目的展现次序，如数值相等则按名称次序排列。取值范围:大于零的整数
-
 
     @JsonFormat(pattern = "yyyy/MM/dd")
     private LocalDateTime addTime; //添加时间
 
-
     @OneToMany
     private List<Section> sections = new ArrayList();
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "course_courseTag", joinColumns = {
-            @JoinColumn(name = "courseId", referencedColumnName = "id")},
-            inverseJoinColumns = {@JoinColumn(name = "courseTagId", referencedColumnName = "id")})
-    private List<CourseTag> courseTags = new ArrayList(); // 所属标签
-
-
     public Course() {
         this.addTime = LocalDateTime.now();
-    }
-
-    public List<CourseTag> getCourseTags() {
-        return courseTags;
-    }
-
-    public void setCourseTags(List<CourseTag> courseTags) {
-        this.courseTags = courseTags;
     }
 
     public String getName() {
@@ -97,7 +78,6 @@ public class Course extends BaseEntity {
     public void setAddTime(LocalDateTime addTime) {
         this.addTime = addTime;
     }
-
 
     public int getSortOrder() {
         return sortOrder;
