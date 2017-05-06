fd51f56bf24538ed062a304f640ed2cf47f57cb5
huangMP <675454967@qq.com>
2017-05-06 14:19:05 +0800

    整合 security 做一些修改

--- src/main/java/com/gcc/course/domain/User.java
+++ src/main/java/com/gcc/course/domain/User.java
@@ -1,12 +1,12 @@
 package com.gcc.course.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by WangZK on 2017/3/20.
@@ -16,7 +16,14 @@ public class User extends BaseEntity {
 
     private String userName;
     private String password;
-    private String authority; //权限
+    private Date lastPasswordResetDate;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "USER_AUTHORITY",
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
+    private List<Authority> authorities;
 
     @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
     private LocalDateTime addTime; //添加用户的时间
@@ -42,16 +49,24 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
-    public String getAuthority() {
-        return authority;
+    public List<Authority> getAuthorities() {
+        return authorities;
     }
 
-    public void setAuthority(String authority) {
-        this.authority = authority;
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
     }
 
     public LocalDateTime getAddTime() {
         return addTime;
+    }
+
+    public Date getLastPasswordResetDate() {
+        return lastPasswordResetDate;
+    }
+
+    public void setLastPasswordResetDate(Date lastPasswordResetDate) {
+        this.lastPasswordResetDate = lastPasswordResetDate;
     }
 
     public void setAddTime(LocalDateTime addTime) {
