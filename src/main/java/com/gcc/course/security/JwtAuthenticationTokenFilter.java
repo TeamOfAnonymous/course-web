08d1cfb91fe3d3bb86960358228133f9bc923592
huangMP <675454967@qq.com>
2017-05-06 14:20:48 +0800

    spring-security 相关类

---
+++ src/main/java/com/gcc/course/security/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,67 @@
+package com.gcc.course.security;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    private final Log logger = LogFactory.getLog(this.getClass());
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("进来了 JwtAuthenticationTokenFilter");
+
+        // 得到 请求头的 认证信息 authToken
+        String authToken = request.getHeader(this.tokenHeader);
+        // authToken.startsWith("Bearer ")
+        // String authToken = header.substring(7);
+
+        // 解析 authToken 得到 用户名
+        String username = jwtTokenUtil.getUsernameFromToken(authToken);
+
+        System.out.println("checking authentication für user " + username);
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            // It is not compelling necessary to load the use details from the database. You could also store the information
+            // in the token and read it from it. It's up to you ;)
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            // For simple validation it is completely sufficient to just check the token integrity. You don't have to call
+            // the database compellingly. Again it's up lto you ;)
+            if (jwtTokenUtil.validateToken(authToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                logger.info("authenticated user " + username + ", setting security context");
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        System.out.println("出去了 JwtAuthenticationTokenFilter");
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/gcc/course/security/JwtUserFactory.java
@@ -0,0 +1,36 @@
+package com.gcc.course.security;
+
+import com.gcc.course.domain.Authority;
+import com.gcc.course.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class JwtUserFactory {
+
+    private JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUserName(),
+                "" , //user.getFirstname(),
+                "" , //user.getLastname(),
+                "" , //user.getEmail(),
+                user.getPassword(),
+                mapToGrantedAuthorities(user.getAuthorities()),
+                !user.isDeleted() , //user.getEnabled(),
+                user.getLastPasswordResetDate()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Authority> authorities) {
+        return authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName().name()))
+                .collect(Collectors.toList());
+    }
+}
