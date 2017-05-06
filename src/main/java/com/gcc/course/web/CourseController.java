4f9ae61c0199c8d92f87bb04b2e540e2638ef18e
WangZK <13434995010@163.com>
2017-05-06 12:30:58 +0800

    commit

--- src/main/java/com/gcc/course/web/CourseController.java
+++ src/main/java/com/gcc/course/web/CourseController.java
@@ -1,11 +1,13 @@
 package com.gcc.course.web;
 
+import com.gcc.course.domain.Article;
 import com.gcc.course.domain.Course;
 import com.gcc.course.service.CourseService;
 import com.gcc.course.service.CourseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,10 +18,23 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/course")
-@Api(value = "课程的后台管理Controller")
+@Api(value = "课程的前台显示Controller")
 public class CourseController {
 
     @Autowired
     private CourseService courseServiceImpl;
+
+    @GetMapping("/getCoursesForPage")
+    @ApiOperation(value = "获取分页的课程列表")
+    public Page<Course> getCoursesForPage(@RequestParam Integer page, @RequestParam Integer size, @RequestParam String searchStr) {
+        return courseServiceImpl.getCoursesForPage(page, size, searchStr);
+    }
+
+    @GetMapping("/get/{id}")
+    @ApiOperation(value = "根据课程id获取课程")
+    public Course getCoursesForPage(@PathVariable String id) {
+        return courseServiceImpl.findById(id);
+    }
+
 
 }
