32f73031e461d5140e243d587697937a075aaf44
huangMP <675454967@qq.com>
2017-05-06 19:01:16 +0800

    springsecurity 相应修改

--- src/main/java/com/gcc/course/service/impl/UserServiceImpl.java
+++ src/main/java/com/gcc/course/service/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.gcc.course.service.impl;
 import com.gcc.course.domain.User;
 import com.gcc.course.repository.UserRepository;
 import com.gcc.course.service.UserService;
+import com.gcc.course.web.dto.WebResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,19 +23,13 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 创建用户
-     *
      * @param user
      * @return
      */
     @Override
-    public boolean save(User user) {
-        try {
-            userRepository.save(user);
-        } catch (Exception e) {
-            return false;
-        } finally {
-            return true;
-        }
+    public WebResult save(User user) {
+        user = userRepository.save(user);
+        return WebResult.ok(user);
     }
 
     /**
@@ -79,14 +75,9 @@ public class UserServiceImpl implements UserService {
      * @return
      */
     @Override
-    public boolean update(User user) {
-        try {
-            userRepository.update(user.getId(), user.getUserName(), user.getPassword(), user.getAuthorities().toString());
-        } catch (Exception e) {
-            return false;
-        } finally {
-            return true;
-        }
+    public WebResult update(User user) {
+        user = userRepository.save(user);
+        return WebResult.ok(user);
     }
 
     /**
@@ -131,6 +122,19 @@ public class UserServiceImpl implements UserService {
         } finally {
             return true;
         }
+    }
+
+    @Override
+    public WebResult updatePassword(String username, String oldPassword, String newPassword) {
+        User user = userRepository.findByUserName(username);
+        if( null != user ){
+            if( user.getPassword().equals( oldPassword.trim() ) ){
+                user.setPassword(newPassword);
+                user = userRepository.save(user);
+                return WebResult.ok(user);
+            }
+        }
+        return new WebResult(500,"用户信息有误",user);
     }
 
 
